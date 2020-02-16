204f1a9020d472a784234d7768dca6ee3d6cc08a
Dural Keys <duralinitaly@gmail.com>
2020-02-16 20:14:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,7 +38,10 @@ end
 def remove_element_from_start_of_array(array)
   
   ["wow", "I", "am", "really", "learning", "arrays!"]
+
 end
+
+
 
 def retrieve_element_from_index(array, index_number)
   
