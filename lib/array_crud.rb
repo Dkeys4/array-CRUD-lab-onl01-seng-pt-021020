fb38d1308ce35c689c7bebb4cd4350100e0204e2
Dural Keys <duralinitaly@gmail.com>
2020-02-16 20:10:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,43 @@
 def create_an_empty_array
   
-end
-
-def create_an_array
+  best_shoes_for_dancing = 
+  
+  []
   
 end
+
+
+def create_an_array 
+  
+  my_favorite_sports = ["golf","tennis","swimming", "nascar"]
+
+end
+
 
 def add_element_to_end_of_array(array, element)
   
+array = ["wow", "I", "am", "really", "learning"] << "arrays!"
+
+  
 end
+
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift "wow" #>> ["I", "am", "really", "learning"]
+  
+
 end
 
+
 def remove_element_from_end_of_array(array)
-  
+   ["wow", "I", "am", "really", "learning", "arrays!"] .pop #>
+
 end
 
 def remove_element_from_start_of_array(array)
   
+  ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_element_from_index(array, index_number)
