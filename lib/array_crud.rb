9f423d007e1fc4831d7170efb09aaa4e3a9bbf0b
Dural Keys <duralinitaly@gmail.com>
2020-02-16 21:06:20 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,69 @@
 def create_an_empty_array
   
-end
-
-def create_an_array
+  best_shoes_for_dancing = 
+  
+  []
   
 end
+
+
+def create_an_array 
+  
+  my_favorite_sports = ["golf","tennis","swimming", "nascar"]
+
+end
+
 
 def add_element_to_end_of_array(array, element)
   
+array = ["wow", "I", "am", "really", "learning"] << "arrays!"
+
+  
 end
+
 
 def add_element_to_start_of_array(array, element)
   
+  array.unshift "wow" #>> ["I", "am", "really", "learning"]
+  
+
 end
 
+
 def remove_element_from_end_of_array(array)
-  
+   ["wow", "I", "am", "really", "learning", "arrays!"] .pop #>
+
 end
 
 def remove_element_from_start_of_array(array)
   
+array.shift # >> ["wow", "I", "am", "really", "learning", "arrays!"]
+
 end
+
+
 
 def retrieve_element_from_index(array, index_number)
   
-end
-
-def retrieve_first_element_from_array(array)
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+ 
+  array [2]
   
 end
 
+
+def retrieve_first_element_from_array(array)
+  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  
+  array [0]
+
+end
+
+
 def retrieve_last_element_from_array(array)
+  ["wow", "I", "am", "really", "learning", "arrays!"]
+  
+  array [5]
   
 end
